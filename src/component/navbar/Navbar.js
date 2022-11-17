b6406968cf35af6f1f7b9c1392b00257b4523266
Jerald-tonmoy-dias <tonmoyjareld@gmail.com>
2022-11-17 18:26:25 +0600

    working on navbar

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from "styled-components";
-
+import { secondaryColor, grayColor } from '../../themeStyles';
 export default function Navbar() {
     const Navbar = styled.nav`
     margin: 16px 0;
@@ -16,13 +16,14 @@ export default function Navbar() {
     list-style: none;
     `;
 
-    const List = styled.li`
+    const List = styled.li(props=>(
+        `
     display: inline-flex;
     flex-basis: 20%;
     position: relative;
     &.active {
-        background-color: #002a6e;
-        color: #002a6e;
+        background-color: ${props.secondaryColor};
+        color: ${props.secondaryColor};
       }
     &.disable {
         &:after, &:before {
@@ -36,19 +37,20 @@ export default function Navbar() {
         }
         &:after {
             bottom: 0;
-            border-bottom-color: #002a6e;
+            border-bottom-color: ${props.secondaryColor};
         }
         &:before {
             top: 0;
-            border-top-color: #002a6e;
+            border-top-color: ${props.secondaryColor};
         }
         padding-left: 5px;
         background-color: #d6d6d6;
         color: #d6d6d6;
       }
-    `;
+    `
+    ));
 
-    const Anchor = styled.a`
+    const Anchor = styled.a(props => (`
     padding: 15px 28px 13px 12px;
     font-size: 17.5px;
     line-height: 1.1;
@@ -56,7 +58,39 @@ export default function Navbar() {
     width: 100%;
     background-color: #fafafa;
     text-decoration: none !important;
-    `;
+    &.is_current {
+        background-color: ${props.secondaryColor};
+        color: ${props.secondaryColor};
+    }
+    &.is_disabled {
+        padding-left: 5px;
+        background-color: #d6d6d6;
+        color: #d6d6d6;
+        &:after,&:before {
+            content: "";
+            display: block;
+            margin-right: -2px;
+            position: absolute;
+            right: 100%;
+            z-index: 1;
+            color: #fafafa;
+            border: 30px solid transparent;
+            border-width: 40px 0 40px 15px;
+        }
+
+        &:before {
+                color: #d6d6d6;
+                top: 0;
+                border-top-color: inherit;
+        }
+        &:after {
+            color: #d6d6d6;
+            bottom: 0;
+            border-bottom-color: inherit;
+        }
+    }
+    }
+    `));
 
     const SpanOne = styled.span`
     color: #fff;
@@ -71,33 +105,63 @@ export default function Navbar() {
 
         <Navbar>
             <OrderList>
-                <List className='active'>
-                    <Anchor >
+                <List
+                    secondaryColor={secondaryColor}
+                    grayColor={grayColor}
+                    className='active'>
+                    <Anchor
+                        secondaryColor={secondaryColor}
+                        grayColor={grayColor}
+                        className='is_current'>
                         <SpanOne>your vehicle</SpanOne>
                         <SpanTwo>1</SpanTwo>
                     </Anchor>
                 </List>
-                <List className='disable'>
-                    <Anchor>
+                <List
+                    secondaryColor={secondaryColor}
+                    grayColor={grayColor}
+                    className='disable'>
+                    <Anchor className="is_disabled"
+                        secondaryColor={secondaryColor}
+                        grayColor={grayColor}
+                    >
                         <SpanOne>your details</SpanOne>
                         <SpanTwo>2</SpanTwo>
                     </Anchor>
                 </List>
-                <List className='disable'>
-                    <Anchor>
+                <List
+                    secondaryColor={secondaryColor}
+                    grayColor={grayColor}
+                    className='disable'>
+                    <Anchor
+                        secondaryColor={secondaryColor}
+                        grayColor={grayColor}
+                        className="is_disabled">
                         <SpanOne> your policy</SpanOne>
                         <SpanTwo>3</SpanTwo>
                     </Anchor>
                 </List>
-                <List className='disable'>
-                    <Anchor>
+                <List
+                    secondaryColor={secondaryColor}
+                    grayColor={grayColor}
+                    className='disable'>
+                    <Anchor
+                        secondaryColor={secondaryColor}
+                        grayColor={grayColor}
+                        className="is_disabled">
                         <SpanOne>your summary</SpanOne>
                         <SpanTwo>4</SpanTwo>
                     </Anchor>
                 </List>
 
-                <List className='disable'>
-                    <Anchor>
+                <List
+                    secondaryColor={secondaryColor}
+                    grayColor={grayColor}
+                    className='disable'>
+                    <Anchor
+                        secondaryColor={secondaryColor}
+                        grayColor={grayColor}
+                        className="is_disabled">
                         <SpanOne> your quotes</SpanOne>
                         <SpanTwo>5</SpanTwo>
                     </Anchor>
