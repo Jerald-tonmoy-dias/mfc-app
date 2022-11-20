4e1035fd16e827c9f413a2b0639438f42ea783fd
Jerald-tonmoy-dias <tonmoyjareld@gmail.com>
2022-11-20 13:31:40 +0600

    working on validation data

--- src/component/form/vehicle/NotFoundVehicle.js
+++ src/component/form/vehicle/NotFoundVehicle.js
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components';
 import { ThemeContext } from 'styled-components';
 
@@ -7,13 +7,29 @@ export default function NotFoundVehicle() {
 
     const Container = styled.div`
         background: ${theme.whiteColor};
-        border: 1px solid ${theme.primaryColor};
+        border: 1px solid ${theme.secondaryColor};
+        padding: 20px;
+        border-radius: 5px;
+        margin: 30px 0;
+    h4 {
+        margin: 0 0 10px 0;
+    }
+        p {
+            margin: 0;
+            span {
+                color: ${theme.primaryColor};
+                font-weight: 700;
+            }
+        }
 
-    `; 
+    `;
 
-  return (
-    <Container>
-        not found
-    </Container>
-  )
+    return (
+        <Container>
+            <h4>Oops! We have a problem</h4>
+            <p>
+              <span>Vehicle Lookup:</span> There were no vehicles found for this registration. Please check that you have entered it correctly
+            </p>
+        </Container>
+    )
 }
--- src/component/form/vehicle/Vehicle.js
+++ src/component/form/vehicle/Vehicle.js
@@ -20,22 +20,31 @@ export default function Vehicle() {
     // local states
     const [openToolTip, setopenToolTip] = useState(false);
     const [modelNo, setModelNo] = useState(null);
+    const [notFound, setnotFound] = useState(false);
 
     // functions
-    const getVehicleInfoFunction = (e,modelNo) => {
+    const getVehicleInfoFunction = (e, modelNo) => {
         e.preventDefault();
         // console.log('show something', e);
         // console.log('show something model no', modelNo);
         setLoading(true);
         const url = BASE_URL + modelNo;
         return axios
-          .get(url)
-          .then((res) => {
-            console.log('response from context value ',res);
-            setvehicleDetails(res.data);
-            setLoading(false);
-          })
-          .catch((err) => {console.log(err)})
+            .get(url)
+            .then((res) => {
+                console.log('response from context value ', res);
+                if(res.data != undefined) {
+                    setvehicleDetails(res.data);
+                    setLoading(false);
+                } else {
+                    setnotFound(true);
+                }
+            })
+            .catch((err) => {
+                setnotFound(true);
+                console.log(err)
+            }
+            )
     };
     return (
         <div>
@@ -44,42 +53,47 @@ export default function Vehicle() {
             >
                 vehicle details
             </Title>
-            <form onSubmit={(e)=>getVehicleInfoFunction(e,modelNo)}>
-                <MainWrapper>
-                    <RegisterCarWrapper bg={theme.liteWhiteColor} color={theme.whiteColor}>
-                        <button onClick={() => setopenToolTip(true)} className='mobile_trigger'>❔</button>
+            {notFound === false ? [
+                   <form onSubmit={(e) => getVehicleInfoFunction(e, modelNo)}>
+                   <MainWrapper>
+                       <RegisterCarWrapper bg={theme.liteWhiteColor} color={theme.whiteColor}>
+                           <button onClick={() => setopenToolTip(true)} className='mobile_trigger'>❔</button>
+   
+                           <label htmlFor="">What's your car's registration?</label>
+                           <div className='inputWrapper'>
+                               <span className='country'>UK</span>
+                               <input type="text" onChange={(e) => setModelNo(e.target.value)} placeholder='Enter registration...' />
+                               <span className="Mark_icon">
+                                   ✏
+                               </span>
+                           </div>
+                           <hr style={{ margin: '30px 0' }} />
+   
+                           <Button type='submit' >
+                               Find vehicle
+                           </Button>
+                       </RegisterCarWrapper>
+                       <ToolTipWrapper borderColor={theme.whiteColor} open={openToolTip} bg={theme.liteBlackColor}>
+                           <SVGElement>
+                               ❔
+                           </SVGElement>
+                           <button onClick={() => setopenToolTip(false)} className='mobile_trigger'>✘</button>
+                           <p className='help-text'>
+                               Entering the registration is the quickest way to get a quote.
+                           </p>
+                           <p>If your car is brand new the registration might not be recognised. Don’t worry we can still compare insurance for you, just enter the details manually - it won’t take long.</p>
+                           <p>
+                               Unfortunately we can’t provide quotes for vehicles manufactured before 1970, motor homes or commercial vehicles weighing more than 3.5 tonnes.
+                           </p>
+                       </ToolTipWrapper>
+                   </MainWrapper>
+               </form>
+            ] :
+            <NotFoundVehicle />
+            }
+         
 
-                        <label htmlFor="">What's your car's registration?</label>
-                        <div className='inputWrapper'>
-                            <span className='country'>UK</span>
-                            <input type="text" onChange={(e) => setModelNo(e.target.value)} placeholder='Enter registration...' />
-                            <span className="Mark_icon">
-                                ✏
-                            </span>
-                        </div>
-                        <hr style={{ margin: '30px 0' }} />
-
-                        <Button type='submit' >
-                            Find vehicle
-                        </Button>
-                    </RegisterCarWrapper>
-                    <ToolTipWrapper borderColor={theme.whiteColor} open={openToolTip} bg={theme.liteBlackColor}>
-                        <SVGElement>
-                            ❔
-                        </SVGElement>
-                        <button onClick={() => setopenToolTip(false)} className='mobile_trigger'>✘</button>
-                        <p className='help-text'>
-                            Entering the registration is the quickest way to get a quote.
-                        </p>
-                        <p>If your car is brand new the registration might not be recognised. Don’t worry we can still compare insurance for you, just enter the details manually - it won’t take long.</p>
-                        <p>
-                            Unfortunately we can’t provide quotes for vehicles manufactured before 1970, motor homes or commercial vehicles weighing more than 3.5 tonnes.
-                        </p>
-                    </ToolTipWrapper>
-                </MainWrapper>
-            </form>
-
-            <NotFoundVehicle/>
+          
 
         </div>
     )
