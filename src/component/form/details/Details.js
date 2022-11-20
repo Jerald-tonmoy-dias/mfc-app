d7577dfd549c615ba70d3a9abdabcd6b6b78cba3
Jerald-tonmoy-dias <tonmoyjareld@gmail.com>
2022-11-20 17:10:47 +0600

    file added

--- src/component/form/details/Details.js
+++ src/component/form/details/Details.js
@@ -16,67 +16,14 @@ export default function Details() {
         setvehicleDetails,
     } = useContext(StoreContext);
 
-    // local states
-    const [openToolTip, setopenToolTip] = useState(false);
-    const [modelNo, setModelNo] = useState(null);
-
-    // functions
-    const getVehicleInfoFunction = (e,modelNo) => {
-        e.preventDefault();
-        // console.log('show something', e);
-        // console.log('show something model no', modelNo);
-        setLoading(true);
-        const url = BASE_URL + modelNo;
-        return axios
-          .get(url)
-          .then((res) => {
-            console.log('response from context value ',res);
-            setvehicleDetails(res.data);
-            setLoading(false);
-          })
-          .catch((err) => {console.log(err)})
-    };
     return (
         <div>
-            {/* <Title
+            <Title
                 color={theme.whiteColor}
             >
                 vehicle details
             </Title>
-            <form onSubmit={(e)=>getVehicleInfoFunction(e,modelNo)}>
-                <MainWrapper>
-                    <RegisterCarWrapper bg={theme.liteWhiteColor} color={theme.whiteColor}>
-                        <button onClick={() => setopenToolTip(true)} className='mobile_trigger'>❔</button>
 
-                        <label htmlFor="">What's your car's registration?</label>
-                        <div className='inputWrapper'>
-                            <span className='country'>UK</span>
-                            <input type="text" onChange={(e) => setModelNo(e.target.value)} placeholder='Enter registration...' />
-                            <span className="Mark_icon">
-                                ✏
-                            </span>
-                        </div>
-                        <hr style={{ margin: '30px 0' }} />
-
-                        <Button type='submit' >
-                            Find vehicle
-                        </Button>
-                    </RegisterCarWrapper>
-                    <ToolTipWrapper borderColor={theme.whiteColor} open={openToolTip} bg={theme.liteBlackColor}>
-                        <SVGElement>
-                            ❔
-                        </SVGElement>
-                        <button onClick={() => setopenToolTip(false)} className='mobile_trigger'>✘</button>
-                        <p className='help-text'>
-                            Entering the registration is the quickest way to get a quote.
-                        </p>
-                        <p>If your car is brand new the registration might not be recognised. Don’t worry we can still compare insurance for you, just enter the details manually - it won’t take long.</p>
-                        <p>
-                            Unfortunately we can’t provide quotes for vehicles manufactured before 1970, motor homes or commercial vehicles weighing more than 3.5 tonnes.
-                        </p>
-                    </ToolTipWrapper>
-                </MainWrapper>
-            </form> */}
 
 
         </div>
